beadd9f537003519f3ebc120f6551ecc4ee097c9
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-26 07:14:58 -0700

    task 2

--- get_op_func.c
+++ get_op_func.c
@@ -10,6 +10,7 @@ void (*get_op_func(char *s))(stack_t**, unsigned int)
 {
 	instruction_t ops[] = {
 		{"push", push}, {"pall", pall}, {"pint", pint},
+		{"pop", pop},
 		{NULL, NULL}
 	};
 	int i = 0;
