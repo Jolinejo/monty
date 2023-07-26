d872e1056fed53a70a06969e2b366e7058668133
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-26 06:06:11 -0700

    task 1

--- functions.c
+++ functions.c
@@ -79,7 +79,7 @@ void pint(stack_t **head, unsigned int line_number)
 {
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		safe_exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*head)->n);
