0ca9683dace5463ccba58493f3fa5fe06df67233
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-26 10:22:15 -0700

    task 12

--- functions_3.c
+++ functions_3.c
@@ -58,11 +58,10 @@ void pstr(stack_t **head, unsigned int line_number)
 	stack_t *temp = *head;
 
 	(void)line_number;
-	if (*head == NULL)
-		return;
 	while (temp != NULL && temp->n >= 32 && temp->n < 127)
 	{
-		printf("%d\n", temp->n);
+		printf("%c", temp->n);
 		temp = temp->next;
 	}
+	printf("\n");
 }
