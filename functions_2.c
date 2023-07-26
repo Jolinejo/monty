aa610337a344e50c574e33264ac04f72c9fbea4a
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-07-26 07:41:04 -0700

    task 4

---
+++ functions_2.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * add - Entry point
+ * Description: print
+ * @head: head
+ * @line_number: number
+ * Return: int
+ */
+void add(stack_t **head, unsigned int line_number)
+{
+	int temp;
+
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		safe_exit(EXIT_FAILURE);
+	}
+	temp = (*head)->n + (*head)->next->n;
+	pop(head, line_number);
+	(*head)->n = temp;
+}
